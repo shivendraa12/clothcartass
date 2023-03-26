9e307b6ff0086854a2aee1abbf85e55641be41b9
shiv <shivendra.singh1919@gmail.com>
2023-03-26 19:17:01 +0530

    changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,16 +5,31 @@ export default function Footer() {
     <div className="footer">
       <div className="footer_left">
         <div className="footer_left_text">
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries....</p>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries....
+          </p>
         </div>
         <div className="footer_left_list">
           <ul>
-            <li>fa</li>
-            <li>tw</li>
-            <li>in</li>
-            <li>yo</li>
-            <li>in</li>
-            <li>yo</li>
+            <li>
+              <img src="/assets/facebook.png" alt="" />
+            </li>
+            <li>
+              <img src="/assets/twiter.png" alt="" />
+            </li>
+            <li>
+              <img src="/assets/insta.png" alt="" />
+            </li>
+            <li>
+              <img src="/assets/linkdin.png" alt="" />
+            </li>
+            <li>
+              <img src="/assets/picon.png" alt="" />
+            </li>
           </ul>
         </div>
       </div>
@@ -23,44 +38,49 @@ export default function Footer() {
         <ul>
           <li>
             <div className="footer_right_list">
-                <h4>ONLINE SHOPPING</h4>
+              <h4>ONLINE SHOPPING</h4>
               <ul>
-                <li>hello1</li>
-
-                <li>hello1</li>
-
-                <li>hello1</li>
-                <li>hello1</li>
-                <li>hello1</li>
-                
+                <li>Men’s T-Shirts</li>
+                <li>Women’s Wear</li>
+                <li>Winter Collections</li>
+                <li>Hooded T-Shirts</li>
+                <li>Streetwear Collections</li>
               </ul>
             </div>
           </li>
           <li>
             <div className="footer_right_list">
-            <h4>CUSTOMER POLICIES </h4>
+              <h4>CUSTOMER POLICIES </h4>
               <ul>
-                <li>hello1</li>
+                <li>About Us </li>
 
-                <li>hello1</li>
+                <li>Terms & Conditions</li>
 
-                <li>hello1</li>
-                <li>hello1</li>
-                <li>hello1</li>
-                
+                <li>Shipping & Returns Policy</li>
+                <li>Cancellation & Refund Policy</li>
+                <li>Contact Us</li>
               </ul>
             </div>
           </li>
           <li>
             <div className="footer_right_list">
-            <h4>STORE INFORMATION</h4>
+              <h4>STORE INFORMATION</h4>
               <ul>
-                <li>hello1</li>
+                <li>
+                  <img src="/assets/location.png" alt="" />
+                  <h5>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</h5>
+                  
+                </li>
 
-                <li>hello1</li>
+                <li>
+                  <img src="/assets/phone.png" alt="" />
+                  <h5>Call Us: 1234567890</h5>
+                </li>
 
-                <li>hello1</li>
-                
+                <li>
+                  <img src="/assets/mail.png" alt="" />
+                  <h5>Email Us: info@yourmail.com</h5>
+                </li>
               </ul>
             </div>
           </li>
